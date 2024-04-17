8d76bfbf56b20a08b0231df7d3213ce4a57ecb53
Alberto <159887450+abcc24@users.noreply.github.com>
2024-04-17 18:11:55 +0200

    Add files via upload

---
+++ es.py
@@ -0,0 +1,31 @@
+x: str="Eric"
+
+print(f"Hello",x,"would you like to learn some Pythontoday?")
+
+####
+
+print(x.lower(),x.upper(),x.title())
+
+###
+
+s='"Sono Batman"'
+print("Batman una volta ha detto " +str(s))
+''
+
+###
+
+famous_person="Batman"
+message='"una volta ha detto"'
+
+print(famous_person + message)
+
+###
+
+filename="python_notes.txt"
+e=filename.removesuffix(".txt")
+print(e)
+
+###
+
+nome="Andrea", "Nicholas", "Rebecca",
+print(nome[0],nome[1],nome[2])
